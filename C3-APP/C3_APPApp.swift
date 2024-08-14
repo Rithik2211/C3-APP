16477c9b1d9b11c46968edeef93fd82dc270dd0d
Rithik2211 <rithikpranao22@gmail.com>
2024-08-14 16:53:45 +0530

    Initial Commit

---
+++ C3-APP/C3_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  C3_APPApp.swift
+//  C3-APP
+//
+//  Created by Rithik Pranao on 14/08/24.
+//
+
+import SwiftUI
+
+@main
+struct C3_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
